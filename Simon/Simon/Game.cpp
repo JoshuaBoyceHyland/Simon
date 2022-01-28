5dc8c3cd69d20c875dacb99bbd2f1414a81fd0c4
c00270917 <c00270917@A307C012>
2022-01-28 12:29:00 +0000

    starting step 4

--- Simon/Simon/Game.cpp
+++ Simon/Simon/Game.cpp
@@ -148,7 +148,7 @@ void Game::setupFontAndText()
 void Game::setUpRectangles()
 {	
 	// red rectangle set up 
-	m_redRectangle.setFillColor(sf::Color(0, 0, 0, 255)); 
+	m_redRectangle.setFillColor(sf::Color(180, 0, 0, 255)); 
 	m_redRectangle.setPosition(200.0f, 200.0f);
 
 	// yellow rectangle set up 
@@ -165,6 +165,6 @@ void Game::setUpRectangles()
 }
 
 /// <summary>
-/// load the texture and setup the sprite for the logo
+/// loads in the triangles 
 /// </summary>
 
